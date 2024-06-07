09266a1a2e72c279dc26d37e6f0b20e99ff7b09b
linhung0319 <linhung0319@gmail.com>
2024-06-07 23:56:53 +0800

    customer and vendor authentication

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,9 @@ app_name = "accounts"
 urlpatterns = [
     path("registerUser/", views.registerUser, name="registerUser"),
     path("registerVendor/", views.registerVendor, name="registerVendor"),
+    path("login/", views.login, name="login"),
+    path("logout/", views.logout, name="logout"),
+    path("myAccount/", views.myAccount, name="myAccount"),
+    path("customerDashboard/", views.customerDashboard, name="customerDashboard"),
+    path("vendorDashboard/", views.vendorDashboard, name="vendorDashboard"),
 ]
