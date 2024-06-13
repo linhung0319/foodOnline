67b35b38f8138f4221376d338c1379ea3adaad94
linhung0319 <linhung0319@gmail.com>
2024-06-13 15:32:41 +0800

    Create menu builder and Categoty CRUD

--- static/js/custom.js
+++ static/js/custom.js
@@ -6,7 +6,7 @@ function initAutoComplete() {
     {
       types: ['geocode', 'establishment'],
       //default in this app is "IN" - add your country code
-      componentRestrictions: { 'country': ['in'] },
+      componentRestrictions: { 'country': ['tw'] },
     })
   // function to specify what should happen when the prediction is clicked
   autocomplete.addListener('place_changed', onPlaceChanged);
@@ -23,4 +23,43 @@ function onPlaceChanged() {
     console.log('place name=>', place.name)
   }
   // get the address components and assign them to the fields
+  // console.log(place)
+  var geocoder = new google.maps.Geocoder();
+  var address = document.getElementById('id_address').value
+
+  geocoder.geocode({ 'address': address }, function (results, status) {
+    // console.log("results=>", results);
+    // console.log("status=>", status);
+    if (status == google.maps.GeocoderStatus.OK) {
+      var latitude = results[0].geometry.location.lat();
+      var longitude = results[0].geometry.location.lng();
+
+      // console.log(latitude);
+      // console.log(longitude);
+      $('#id_latitude').val(latitude);
+      $('#id_longitude').val(longitude);
+
+      $('#id_address').val(address);
+    }
+  });
+
+  // loop through the address components and
+  // assign them to the fields
+  console.log(place.address_components)
+  for (var i = 0; i < place.address_components.length; i++) {
+    for (var j = 0; j < place.address_components[i].types.length; j++) {
+      if (place.address_components[i].types[j] == 'country') {
+        console.log(place.address_components[i].long_name)
+        $('#id_country').val(place.address_components[i].long_name);
+      }
+      if (place.address_components[i].types[j] == 'administrative_area_level_1') {
+        console.log(place.address_components[i].long_name)
+        $('#id_city').val(place.address_components[i].long_name);
+      }
+      if (place.address_components[i].types[j] == 'postal_code') {
+        console.log(place.address_components[i].long_name)
+        $('#id_pin_code').val(place.address_components[i].long_name);
+      }
+    }
+  }
 }
--- vendor/views.py
+++ vendor/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required, user_passes_test
+from django.template.defaultfilters import slugify
 
 from .forms import VendorForm
 from .models import Vendor
 from accounts.forms import UserProfileForm
 from accounts.models import UserProfile
 from accounts.views import check_role_vendor
+from menu.models import Category, FoodItem
+from menu.forms import CategoryForm
+
+
+def get_vendor(request):
+    vendor = Vendor.objects.get(user=request.user)
+    return vendor
 
 
 @login_required(login_url="accounts:login")
@@ -37,3 +45,76 @@ def vprofile(request):
         "vendor": vendor,
     }
     return render(request, "vendor/vprofile.html", context)
+
+
+@login_required(login_url="accounts:login")
+@user_passes_test(check_role_vendor)
+def menu_builder(request):
+    vendor = get_vendor(request)
+    categories = Category.objects.filter(vendor=vendor).order_by("created_at")
+    context = {
+        "categories": categories,
+    }
+    return render(request, "vendor/menu_builder.html", context)
+
+
+@login_required(login_url="accounts:login")
+@user_passes_test(check_role_vendor)
+def fooditems_by_category(request, pk=None):
+    vendor = get_vendor(request)
+    category = get_object_or_404(Category, pk=pk)
+    fooditems = FoodItem.objects.filter(vendor=vendor, category=category)
+    context = {
+        "fooditems": fooditems,
+        "category": category,
+    }
+    return render(request, "vendor/fooditems_by_category.html", context)
+
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            category_name = form.cleaned_data["category_name"]
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            category.slug = slugify(category_name)
+            form.save()
+            messages.success(request, "Category added successfully.")
+            return redirect("accounts:vendor:menu_builder")
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+    context = {
+        "form": form
+    }
+    return render(request, "vendor/add_category.html", context)
+
+def edit_category(request, pk=None):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == "POST":
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            category_name = form.cleaned_data["category_name"]
+            category = form.save(commit=False)
+            category.vendor = get_vendor(request)
+            category.slug = slugify(category_name)
+            form.save()
+            messages.success(request, "Category updated successfully.")
+            return redirect("accounts:vendor:menu_builder")
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm(instance=category)
+    context = {
+        "form": form,
+        "category": category,
+    }
+    return render(request, "vendor/edit_category.html", context)
+
+def delete_category(request, pk=None):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    messages.success(request, "Category has been deleted successfully!")
+    return redirect("accounts:vendor:menu_builder")
