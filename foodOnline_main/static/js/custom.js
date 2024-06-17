8c2d2c2e2f0e3de08dc89c5cffeaadeb8fae5201
linhung0319 <linhung0319@gmail.com>
2024-06-17 21:35:45 +0800

    Implement Cart functionailities with AJAX request

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -63,3 +63,62 @@ function onPlaceChanged() {
     }
   }
 }
+
+
+$(document).ready(function () {
+  $('.add_to_cart').on('click', function (e) {
+    e.preventDefault();
+
+    food_id = $(this).attr('data-id');
+    url = $(this).attr('data-url');
+
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: function (response) {
+        if (response.status == 'login_required') {
+          swal(response.message, '', 'info').then(function () {
+            window.location = '/login';
+          });
+        } else if (response.status == 'Failed') {
+          swal(response.message, '', 'error');
+        } else {
+          $('#cart_counter').html(response.cart_counter['cart_count']);
+          $('#qty-' + food_id).html(response.qty);
+        }
+      }
+    })
+  })
+
+  // Place the cart item quantity on load
+  $('.item_qty').each(function () {
+    var the_id = $(this).attr('id');
+    var qty = $(this).attr('data-qty');
+    $('#' + the_id).html(qty);
+  })
+
+  // Decrease cart
+  $('.decrease_cart').on('click', function (e) {
+    e.preventDefault();
+
+    food_id = $(this).attr('data-id');
+    url = $(this).attr('data-url');
+
+    $.ajax({
+      type: 'GET',
+      url: url,
+      success: function (response) {
+        if (response.status == 'login_required') {
+          swal(response.message, '', 'info').then(function () {
+            window.location = '/login';
+          });
+        } else if (response.status == 'Failed') {
+          swal(response.message, '', 'error');
+        } else {
+          $('#cart_counter').html(response.cart_counter['cart_count']);
+          $('#qty-' + food_id).html(response.qty);
+        }
+      }
+    })
+  })
+});
