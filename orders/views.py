ee7c84309be292ff249df4e3f9acbb40083561aa
linhung0319 <linhung0319@gmail.com>
2024-07-01 21:22:42 +0800

    Place order and generate order number

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+import json
 
-# Create your views here.
+from django.shortcuts import render, redirect
+
+from marketplace.models import Cart
+from marketplace.context_processors import get_cart_amounts
+from .forms import OrderForm
+from .models import Order
+from .utils import generate_order_number
+
+
+def place_order(request):
+    cart_items = Cart.objects.filter(user=request.user).order_by("created_at")
+    cart_count = cart_items.count()
+    if cart_count <= 0:
+        return redirect("marketplace:marketplace")
+    
+    cart_amounts = get_cart_amounts(request)
+    subtotal = cart_amounts["subtotal"]
+    total_tax = cart_amounts["tax"]
+    grand_total = cart_amounts["grand_total"]
+    tax_data = cart_amounts["tax_dict"]
+
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = Order()
+            order.first_name = form.cleaned_data["first_name"]
+            order.last_name = form.cleaned_data["last_name"]
+            order.phone = form.cleaned_data["phone"]
+            order.email = form.cleaned_data["email"]
+            order.address = form.cleaned_data["address"]
+            order.country = form.cleaned_data["country"]
+            order.state = form.cleaned_data["state"]
+            order.city = form.cleaned_data["city"]
+            order.pin_code = form.cleaned_data["pin_code"]
+            order.user = request.user
+            order.total = grand_total
+            order.tax_data = json.dumps(tax_data)
+            order.total_tax = total_tax
+            order.payment_method = request.POST["payment_method"]
+            order.save() # order id is generated here
+            order.order_number = generate_order_number(order.id)
+            order.save()
+            return redirect("orders:place_order")
+        else:
+            print(form.errors)
+
+    return render(request, "orders/place_order.html")
